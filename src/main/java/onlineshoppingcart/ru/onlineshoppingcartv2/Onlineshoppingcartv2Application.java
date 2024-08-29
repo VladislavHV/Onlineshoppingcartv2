61b227bac1cb17712dfa154fd1020ad7f9d8642e
origin <kruglov56832@mail.ru>
2024-08-29 11:41:16 +0900

    Onlineshoppingcartv2

---
+++ src/main/java/onlineshoppingcart/ru/onlineshoppingcartv2/Onlineshoppingcartv2Application.java
@@ -0,0 +1,13 @@
+package onlineshoppingcart.ru.onlineshoppingcartv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Onlineshoppingcartv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Onlineshoppingcartv2Application.class, args);
+	}
+
+}
